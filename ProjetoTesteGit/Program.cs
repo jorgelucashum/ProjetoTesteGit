56931469b486a736e9126b0da9883675e99ffcf1
Jorge Lucas <jorgelucashum@gmail.com>
2021-03-27 17:08:27 -0300

    Projeto iniciado

---
+++ ProjetoTesteGit/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoTesteGit
+{
+    class Program
+    {
+        //Git: Criando e configurando o controle de versão.
+        static void Main(string[] args)
+        {
+            // Após instalar o 'Git' o primeiro passo é criar uma '.txt' no local do projeto chamado '.gitignore' e pesquisar as coisas que serão ignoradas para colar lá dentro.
+            // Após finalizado esse arquivo inicie um 'gitbash' dentro da pasta do projeto para abrir o console. // o comando 'git' irá auxiliar em alguns comandos basicos.
+            // O comando para inicializar o controle de versão na pasta é o: 'git init'.
+            // O Comando para configurar o nome e email que fez a alterção no git: git config --global user.name "nome" & git config --global user.email "email@.com"
+
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+    }
+}
