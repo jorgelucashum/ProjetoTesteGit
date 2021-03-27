45f3adb5245fd687a2d0c103fbb06fe78873a4d7
Jorge Lucas <jorgelucashum@gmail.com>
2021-03-27 17:11:09 -0300

    Acrescentada uma mensagem de bom dia

--- ProjetoTesteGit/Program.cs
+++ ProjetoTesteGit/Program.cs
@@ -13,10 +13,15 @@ namespace ProjetoTesteGit
         {
             // Após instalar o 'Git' o primeiro passo é criar uma '.txt' no local do projeto chamado '.gitignore' e pesquisar as coisas que serão ignoradas para colar lá dentro.
             // Após finalizado esse arquivo inicie um 'gitbash' dentro da pasta do projeto para abrir o console. // o comando 'git' irá auxiliar em alguns comandos basicos.
-            // O comando para inicializar o controle de versão na pasta é o: 'git init'.
-            // O Comando para configurar o nome e email que fez a alterção no git: git config --global user.name "nome" & git config --global user.email "email@.com"
+            // 'git init' : Comando para inicializar o controle de versão na pasta é o: 'git init'.
+            // 'git config --global user.name "nome"' & 'git config --global user.email "email@.com"' : Comando para configurar o nome e email que fez a alterção no git.
+            // 'git status' : Verifica como está o repositório. // cor vermelha = Arquivos não rastreados; Verde: Arqquivos pronto para serem commitados (stage).
+            // 'git add .' : (sendo o '.' todos os arquivos, também podem ser adicionados somente alguns arquivos.) - Adiciona os arquivos para a área de stage
+            // 'git commit -m "descrição aqui"' : "Commita" os arquivos
+            // 'git log' : Mostra os commits feitos com seus devidos registros
 
             Console.WriteLine("Hello World");
+            Console.WriteLine("bom dia");
             Console.ReadLine();
         }
     }
