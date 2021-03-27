04035ace7ee5566bcd54c8335d9b0a289f3df2a3
Jorge Lucas <jorgelucashum@gmail.com>
2021-03-27 17:18:27 -0300

    Mensagem adicionada

--- ProjetoTesteGit/Program.cs
+++ ProjetoTesteGit/Program.cs
@@ -15,13 +15,16 @@ namespace ProjetoTesteGit
             // Após finalizado esse arquivo inicie um 'gitbash' dentro da pasta do projeto para abrir o console. // o comando 'git' irá auxiliar em alguns comandos basicos.
             // 'git init' : Comando para inicializar o controle de versão na pasta é o: 'git init'.
             // 'git config --global user.name "nome"' & 'git config --global user.email "email@.com"' : Comando para configurar o nome e email que fez a alterção no git.
-            // 'git status' : Verifica como está o repositório. // cor vermelha = Arquivos não rastreados; Verde: Arqquivos pronto para serem commitados (stage).
-            // 'git add .' : (sendo o '.' todos os arquivos, também podem ser adicionados somente alguns arquivos.) - Adiciona os arquivos para a área de stage
-            // 'git commit -m "descrição aqui"' : "Commita" os arquivos
-            // 'git log' : Mostra os commits feitos com seus devidos registros
+            // 'git status' : Verifica como está o repositório. // cor vermelha = Arquivos não rastreados; Verde: Arquivos pronto para serem commitados (stage).
+            // 'git add .' : (sendo o '.' todos os arquivos, também podem ser adicionados somente alguns arquivos.) - Adiciona os arquivos para a área de stage.
+            // 'git commit -m "descrição aqui"' : "Commita" os arquivos.
+            // 'git log' : Mostra os commits feitos com seus devidos registros.
+            // 'git log --oneline' : Mostra os commits resumidos
 
             Console.WriteLine("Hello World");
             Console.WriteLine("bom dia");
+
+            Console.WriteLine("Mensagem");
             Console.ReadLine();
         }
     }
